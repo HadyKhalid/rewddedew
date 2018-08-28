726abd547743abf1163657c493058526a9ea9473
mohelhaj <42706914+mohelhaj@users.noreply.github.com>
2018-08-28 11:39:03 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -28,56 +28,69 @@ client.user.setGame(`playing`,"3K")
   console.log('')
 });
 
-var cats = [
-
-"https://cdn.discordapp.com/attachments/479044877745782801/479045993132720128/download_1.jpg",
-"https://cdn.discordapp.com/attachments/479044877745782801/479046003937247253/download_2.jpg",
-"https://cdn.discordapp.com/attachments/479044877745782801/479046011956887552/download_3.jpg",
-"https://cdn.discordapp.com/attachments/479044877745782801/479046019904962562/download_4.jpg",
-"https://cdn.discordapp.com/attachments/479044877745782801/479046030214692864/download_5.jpg",
-"https://cdn.discordapp.com/attachments/479044877745782801/479046039794352151/download_6.jpg",
-"https://cdn.discordapp.com/attachments/479044877745782801/479046055581974549/download_7.jpg",
-"https://cdn.discordapp.com/attachments/479044877745782801/479046065367154712/download.jpg",
-"https://cdn.discordapp.com/attachments/479044877745782801/479046084442980354/images_1.jpg",
-"https://cdn.discordapp.com/attachments/479044877745782801/479046105674547216/images_4.jpg",
-"https://cdn.discordapp.com/attachments/479044877745782801/479046113945583639/images_5.jpg",
-"https://cdn.discordapp.com/attachments/479044877745782801/479046119838580756/images_6.jpg",
-"https://cdn.discordapp.com/attachments/479044877745782801/479046131796672513/images_7.jpg",
-"https://cdn.discordapp.com/attachments/479044877745782801/479046143318425621/images_8.jpg",
-"https://cdn.discordapp.com/attachments/479044877745782801/479046152491368448/images_9.jpg",
-"https://cdn.discordapp.com/attachments/479044877745782801/479046158979825686/images_10.jpg",
-"https://cdn.discordapp.com/attachments/479044877745782801/479046162247188498/images_11.jpg",
-"https://cdn.discordapp.com/attachments/479044877745782801/479046196434960416/images_12.jpg",
-"https://cdn.discordapp.com/attachments/479044877745782801/479046214571130882/images_13.jpg",
-"https://cdn.discordapp.com/attachments/479044877745782801/479046219528929290/images_14.jpg",
-"https://cdn.discordapp.com/attachments/479044877745782801/479072892022161408/images_15.jpg",
-"https://cdn.discordapp.com/attachments/479044877745782801/479046222045511685/images_16.jpg",
-"https://cdn.discordapp.com/attachments/479044877745782801/479046225086382081/images_18.jpg",
-"https://cdn.discordapp.com/attachments/479044877745782801/479046228047560736/images_19.jpg",
-"https://cdn.discordapp.com/attachments/479044877745782801/479046230794829834/images_20.jpg",
-"https://cdn.discordapp.com/attachments/479044877745782801/479046233533710336/images_21.jpg",
-"https://cdn.discordapp.com/attachments/479044877745782801/479074274850766863/images_22.jpg",
-"https://cdn.discordapp.com/attachments/479044877745782801/479046289477337089/images_23.jpg",
-"https://cdn.discordapp.com/attachments/479044877745782801/479046293436629041/images_24.jpg",
-"https://cdn.discordapp.com/attachments/479044877745782801/479046294925606914/images_25.jpg",
-"https://cdn.discordapp.com/attachments/479044877745782801/479046303330992169/images_26.jpg",
-"https://cdn.discordapp.com/attachments/479044877745782801/479046307072311317/images_27.jpg",
-"https://cdn.discordapp.com/attachments/479044877745782801/479046315100340224/images_28.jpg",
-"https://cdn.discordapp.com/attachments/479044877745782801/479046325619392537/images_29.jpg",
-"https://cdn.discordapp.com/attachments/479044877745782801/479046333114875907/images_30.jpg",
-"https://cdn.discordapp.com/attachments/479044877745782801/479046334825889807/images_31.jpg",
-"https://cdn.discordapp.com/attachments/479044877745782801/479046338718466058/images_32.jpg",
-"https://cdn.discordapp.com/attachments/479044877745782801/479046340379148288/images_33.jpg",
-"https://cdn.discordapp.com/attachments/479044877745782801/479046342795198484/images_34.jpg",
-"https://cdn.discordapp.com/attachments/479044877745782801/479046349644365827/images.jpg"
-]
-    client.on('message', message => {
-        var args = message.content.split(" ").slice(1);
-    if(message.content.startsWith(prefix + '-هل تعلم')) {
-         var cat = new Discord.RichEmbed()
-.setImage(cats[Math.floor(Math.random() * cats.length)])
-message.channel.sendEmbed(cat);
-    }
+console.log('mariam ra7t tmot al nas');
+client.on('ready', () => {
+  console.log(`im redey`);
 });
+const zead = [
+   '*** انا اسمي مريم ***',
+   '*** مرحباَ ماهو اسمك ؟ ***',
+   `*** اهلا بك ! انا تائهه في هذا المكان  ***`,
+   '*** هل تود مساعدتي ؟ ***',
+   '*** لماذا هل انت قاسي القلب ؟ ***',
+   '*** انني اشفق عليك عليك يجب ان تطهر روحك وتحب الخير للجميع ***',
+   '*** ابتعد عني قليل انني متعبة ***',
+   '*** هل انت نادم على ماقلت ؟ ***',
+   '*** ابتعد عني قليل انني متعبة ***',
+   '*** هل انت نادم على ماقلت ؟ ***',
+   '*** هل تود مساعدتي ؟ ***',
+   '*** واو اشكرك انك شخصاَ رائع ! ***',
+   '*** ابحث معي عن منزلي لقد كان قريباَ من هنا ***',
+   '*** ولاكن عندما حل الليل لم اعد ارى اي شيء ***',
+   '*** مذا تظن اين يوجد ؟ يمين او يسار ***',
+   '*** هيا اذاَ ***',
+   '*** اود ان اسئلك سؤال ونحن في الطريق ***',
+   '*** هل تراني فتاة لطيفة ام مخيفة ***',
+   '*** اشكرك !  ***',
+   '*** لقد وصلنا الى المنزل شكراَ جزيلَ انتطرني ثواني وسوف اعود ***',
+   '*** هل انت جاهز ؟ ***',
+   '*** لقد اخبرت والدي عنك وهم متحمسين لرؤيتك ***',
+   '*** هل تود ان تراهم الان ***',
+'*** انا لست الحوت الازرق كما يدعون ***',
+   '*** انا لست كاذبة صدقني***',
+   '*** لماذا ارى في عينيك الخوف ؟ ***',
+   '*** انا مجرد فتاة لطيفة تحب اللعب مع الجميع ***',
+   '*** اعرف كل شيء يحدث اسمع ذالك بالراديو ***',
+   '*** سمعت ان البشر يقتلون من اجل المال فقط ***',
+   '*** لماذا لم تدخل الغرفة ؟ ***',
+   '*** ههههههههههههههههههه انت الان مسجون في هذه الغرفة ***',
+   '*** لن تخرج حتى اعود لك بعد قليل ***',
+   '*** المفتاح معك ! اكتب .مريم  ***',
+   '*** مفتاح احمر , هل حصلت عليه ؟ ***',
+   '*** ان لم تحصل عليه , اكتب .مريم مرة اخرى ***',
+   '*** مفتاح اسود . هل حصلت عليه ؟ ***',
+   '*** اين تريد ان تختبئ بسرعة قبل ان تعود ***',
+   '*** لقد عادت من جديد الى المنزل ***',
+   '*** لا تصدر اي صوت ! ***',
+   '*** مريم : لقد عدت ***',
+   '*** مريم : يا ايها المخادع اين انت ***',
+   '*** مريم : اعلم انك هنا في المنزل ***',
+   '*** مريم : ماذا تريد ان تسمع ***',
+   '*** مريم : اضغط على الرابط اهداء مني لك | https://www.youtube.com/watch?v=hvSiuQccmtg ***',
+   '*** احد ما خرج من المنزل ***',
+   '*** انتظر الجزء الثاني عندما يوصل البوت 100 سيرفر , ساعدنا في نشر البوت وادخل هذا السيرفر  ***'
+]
+ client.on('message', message => {
+ if (message.content.startsWith('!مريم')) {
+  var mariam= new Discord.RichEmbed()
+  .setTitle("لعبة مريم ..")
+  .setColor('RANDOM')
+  .setDescription(`${zead[Math.floor(Math.random() * zead.length)]}`)
+  .setImage("https://www.npa-ar.com/wp-content/uploads/2017/08/%D9%84%D8%B9%D8%A8%D8%A9-%D9%85%D8%B1%D9%8A%D9%85-300x200.jpg")
+   message.channel.sendEmbed(mariam);
+   message.react("??")
+  }
+});
+
 
 client.login(process.env.BOT_TOKEN);
