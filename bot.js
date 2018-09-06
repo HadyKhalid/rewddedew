480778d4b403bec6ee9c82a05b4265d32ed9ac1e
mohelhaj <42706914+mohelhaj@users.noreply.github.com>
2018-09-06 21:15:26 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -312,7 +312,7 @@ const Sra7a = [
 	 'صراحه  |  ما هي أمنياتك المُستقبلية؟‏',
 ]
    client.on('message', message => {
- if (message.content.startsWith(prefix + "صراحة")) {
+ if (message.content.startsWith(prefix + "!صراحة")) {
      if(!message.channel.guild) return message.reply('** This command only for servers **');
   var client= new Discord.RichEmbed()
   .setTitle("لعبة صراحة ..")
